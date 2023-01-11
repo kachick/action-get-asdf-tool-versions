f55d372b135b37d41082ac1ccd2ecd9a568467c3
Kenichi Kamiya <kachick1@gmail.com>
2023-01-11 19:33:23 +0900

    Be scripts

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,9 @@
+#!/usr/bin/env cabal
+{- cabal:
+build-depends: base >= 4.7 && < 5
+            , action-parse-asdf-tool-versions
+-}
+
 module Main where
 import Asdf.Parser (parseAsdf, toJson)
 import Data.Function ((&))
