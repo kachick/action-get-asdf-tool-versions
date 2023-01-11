8f403a27f0dbda274338c918302cdbdfb53d05bd
Kenichi Kamiya <kachick1@gmail.com>
2023-01-11 22:42:50 +0900

    WIP

--- src/Asdf/Parser.hs
+++ src/Asdf/Parser.hs
@@ -5,23 +5,24 @@ module Asdf.Parser (parseAsdf, toJson) where
 import Data.List (intercalate)
 import Text.Printf (printf)
 import Data.Either (rights)
+import Data.List.NonEmpty as NE
 
-data Entry = Entry{toolname :: String, versions :: [String] }
+data Entry = Entry{toolname :: String, versions :: NonEmpty String }
 
 -- Avoided to depend external libraries as aeson and/or parsec
 
 entry :: [String] -> Either String Entry
 entry cells = case cells of
-        x:xs -> Right Entry{ toolname = x, versions = xs }
+        t:v1:vs -> Right Entry{ toolname = t, versions = fromList (v1:vs) }
         _ -> Left "empty versions"
 
 parseAsdf :: String -> [Entry]
-parseAsdf content =  rights [ entry x | x <- map words (filter (/= []) (map (takeWhile (/= '#')) (lines content)))]
+parseAsdf content =  rights [ entry x | x <- Prelude.map words (Prelude.filter (/= []) (Prelude.map (Prelude.takeWhile (/= '#')) (lines content)))]
 
 formatEntry :: Entry -> String
-formatEntry e = printf "\"%s\":\"%s\"" (toolname e) (head (versions e))
+formatEntry e = printf "\"%s\":\"%s\"" (toolname e) (NE.head (versions e))
 
 -- No accurate for some special characters
 toJson :: [Entry] -> String
-toJson entries = printf "{%s}" (intercalate "," (map formatEntry entries))
+toJson entries = printf "{%s}" (intercalate "," (Prelude.map formatEntry entries))
 
