85037241352b634fa86a033cced11bbe98a5f348
Kenichi Kamiya <kachick1@gmail.com>
2023-01-12 19:32:46 +0900

    Less side effect

--- lib/asdf.rb
+++ lib/asdf.rb
@@ -4,12 +4,10 @@ module Asdf
   # @param [String] body
   # @return [Hash<String, String>]
   def self.parse_primaries(body)
-    body.each_line.each_with_object({}) do |line, outputs|
+    body.each_line.map do |line|
       comment_trimmed = line.slice(/[^#]+/)
       tool_name, primary_version, *_rest_versions = comment_trimmed.split
-      if tool_name && primary_version
-        outputs[tool_name] = primary_version
-      end
-    end
+      (tool_name && primary_version) && [tool_name, primary_version]
+    end.compact.to_h
   end
 end
