fc70d9971a3f9b0d6fd4a5eb6d028a882d505403
Kenichi Kamiya <kachick1@gmail.com>
2023-01-12 19:37:30 +0900

    Add extended hugo for example

--- test/asdf_test.rb
+++ test/asdf_test.rb
@@ -11,10 +11,13 @@ class TestAsdf < Test::Unit::TestCase
 
   nodejs 18.11.0 16.17.1 # This entry exists for test
   shfmt 3.6.0
+
+  # versions might include non semver
+  hugo extended_0.109.0 0.105.0
 EOD
 
   def test_parse_primaries
-    assert_equal({'nodejs'=>'18.11.0', 'shellcheck'=>'0.9.0', 'shfmt'=>'3.6.0'}, Asdf.parse_primaries(EXAMPLE))
+    assert_equal({'nodejs'=>'18.11.0', 'shellcheck'=>'0.9.0', 'shfmt'=>'3.6.0', 'hugo'=>'extended_0.109.0'}, Asdf.parse_primaries(EXAMPLE))
     assert_equal({}, Asdf.parse_primaries('invalid'))
   end
 end
