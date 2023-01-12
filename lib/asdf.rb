fd89285c8be3aa9471aded2c4915e31416a4dc8c
Kenichi Kamiya <kachick1@gmail.com>
2023-01-12 19:40:13 +0900

    Fix to handle comments

--- lib/asdf.rb
+++ lib/asdf.rb
@@ -5,8 +5,8 @@ module Asdf
   # @return [Hash<String, String>]
   def self.parse_primaries(body)
     body.each_line.map do |line|
-      comment_trimmed = line.slice(/[^#]+/)
-      tool_name, primary_version, *_rest_versions = comment_trimmed.split
+      comment_trimmed = line.slice(/^[^#]+/)
+      tool_name, primary_version, *_rest_versions = comment_trimmed&.split
       (tool_name && primary_version) && [tool_name, primary_version]
     end.compact.to_h
   end
--- test/asdf_test.rb
+++ test/asdf_test.rb
@@ -5,16 +5,16 @@ require 'test/unit'
 require_relative '../lib/asdf'
 
 class TestAsdf < Test::Unit::TestCase
-  EXAMPLE =<<'EOD'
-  shellcheck 0.9.0 0.7.2
-  # This line and following line is comment
+  EXAMPLE =<<'ASDF'
+shellcheck 0.9.0 0.7.2
+# This line and following line is comment
 
-  nodejs 18.11.0 16.17.1 # This entry exists for test
-  shfmt 3.6.0
+nodejs 18.11.0 16.17.1 # This entry exists for test
+shfmt 3.6.0
 
-  # versions might include non semver
-  hugo extended_0.109.0 0.105.0
-EOD
+# versions might include non semver
+hugo extended_0.109.0 0.105.0
+ASDF
 
   def test_parse_primaries
     assert_equal({'nodejs'=>'18.11.0', 'shellcheck'=>'0.9.0', 'shfmt'=>'3.6.0', 'hugo'=>'extended_0.109.0'}, Asdf.parse_primaries(EXAMPLE))
